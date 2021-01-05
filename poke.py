413c095832214509074b85d11d8a428cf6662767
victor <victorced13@gmail.com>
2021-01-05 13:58:41 -0400

    primer commit

---
+++ poke.py
@@ -0,0 +1,87 @@
+import requests, json
+
+def primeraRespuesta():
+    url = "https://pokeapi.co/api/v2/pokemon?limit=2000&offset=0"
+
+    response  = requests.get(url)
+    response  = response.json()
+    response  = response.get('results', {})
+    filtro=[nun for nun in response if 'at' in nun['name'] and nun['name'].count('a')==2]
+    print  len(filtro)
+    
+
+def segundarepuesta():
+    url = "https://pokeapi.co/api/v2/egg-group/ground"
+    response  = requests.get(url)
+    response  = response.json()
+    response  = response.get('pokemon_species', {})
+    url2 = "https://pokeapi.co/api/v2/egg-group/fairy"
+    response2  = requests.get(url2)
+    response2  = response2.json()
+    response2  = response2.get('pokemon_species', {})
+    response.extend([element for element in response2 if element not in response])
+    cantidad =len(response)
+    print cantidad
+    
+    
+def tercerarepuesta():
+    url='https://pokeapi.co/api/v2/generation/1'
+    response  = requests.get(url)
+    response  = response.json()
+    response  = response.get('pokemon_species', {})
+    response2 = requests.get('https://pokeapi.co/api/v2/type/2')
+    response2  = response2.json()
+    response2  = response2.get('pokemon', {})
+    
+    peso=[0,0]
+    filtro =[valor for valor in response2 
+                    for valor2 in response if valor2['name'] == valor['pokemon']['name']]
+
+    for elem in filtro:
+        response3  = requests.get(elem['pokemon']['url'])
+        response3  = response3.json()
+        response3  = response3.get('weight', {})
+        if peso[0] <= response3:
+             peso[0] = response3
+        if peso[1] >= response3 or 0 == peso[1]:
+             peso[1] = response3
+
+    print peso
+
+
+primeraRespuesta()
+segundarepuesta()
+tercerarepuesta()
+
+
+       
+
+            
+
+        
+        
+    
+
+
+ 
+
+
+
+     
+
+    
+
+    
+ 
+    
+   
+
+
+
+
+
+
+
+
+
+
